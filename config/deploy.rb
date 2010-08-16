b29d90589f4b1a5d47fb39c9581691be9599b446
Scott White <scott@3rdarm.com>
2010-08-15 23:18:14 -0400

    yea keep forgetting

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "pipes"
+set :repository, "git@github.com:scottwhite/pipes.git"
+set :scm, "git"
+set :branch, "master"
+# set :deploy_via, :remote_cache
+
+set :deploy_via, :copy
+set :copy_strategy, :export
+set :copy_exclude, ["config/deploy.rb","lib/tasks/rspec.rake" "doc", "spec"]
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+role :web, "web01.pipes.io"
+role :app, "web01.pipes.io"
+role :db,  "web01.pipes.io", :primary => true
+set :deploy_to,"/var/www/#{application}"
+set :user, 'pipes'
+set :use_sudo, false
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+desc "Create the symbolic links from the public www dir to the server install of Ext JS and the pdfs dir"
+task :symbolic_links do
+  run "ln -s /var/www/pipes/current_provider_login #{current_release}/config/"
+end
+
+after "deploy:symlink", :symbolic_links
