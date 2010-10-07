7f055ad6e7098cd7a58ed6be01e655cfa39f40e8
Scott White <scott@3rdarm.com>
2010-10-06 22:57:41 -0400

    adding view

---
+++ db/migrate/20101007020604_did_mappings_view.rb
@@ -0,0 +1,17 @@
+class DidMappingsView < ActiveRecord::Migration
+  def self.up
+    connection.execute(%Q{CREATE VIEW `did_mappings` AS 
+        select `d`.`phone_number` AS `phone_number`,
+        `up`.`number` AS `number`,
+        `d`.`usage_state` AS `usage_state`,
+        `dup`.`current_usage` AS `current_usage`,
+        (1200 - current_usage) as 'time_left'
+        from `dids` `d` 
+        join `dids_user_phones` `dup` on `dup`.`did_id` = `d`.`id`
+        join `user_phones` `up` on `up`.`id` = `dup`.`user_phone_id`})
+  end
+
+  def self.down
+    connection.execute("drop view did_mappings")
+  end
+end
