1c0531de177e67d41810798ca5b93e62fce8b1b6
Scott White <scott@3rdarm.com>
2011-02-09 22:28:13 -0500

    more fun

--- db/migrate/20110202044221_add_trigger_call_queue.rb
+++ db/migrate/20110202044221_add_trigger_call_queue.rb
@@ -1,17 +1,49 @@
 class AddTriggerCallQueue < ActiveRecord::Migration
   def self.up
+    add_column :dids_user_phones, :expired, :boolean, default: false, null: false
+    add_column :call_queue, :queue_type, :int, default: 1, null: false
+    
+    connection.execute("drop view did_mappings")
+    
+    connection.execute(%Q{CREATE VIEW `did_mappings` AS 
+        select `d`.`phone_number` AS `phone_number`,
+        `up`.`number` AS `number`,
+        `dup`.`current_usage` AS `current_usage`,
+        (1200 - current_usage) as 'time_left'
+        from `dids` `d` 
+        join `dids_user_phones` `dup` on `dup`.`did_id` = `d`.`id`
+        and dup.expired = 0
+        join `user_phones` `up` on `up`.`id` = `dup`.`user_phone_id`
+        where usage_state = 2})
+    
     self.execute(%Q{
       create trigger insert_call_queue after insert on cdr
         for each row begin
           set @email = null;
           set @start_date = null;
           set @time_left = null;
-          select (1200 - current_usage) as time_left, dids_user_phones.user_phone_id, dids_user_phones.created_at
-          into @time_left, @user_phone_id, @start_date
+          set @source = null;
+          set @expired = null;
+          set @queue_type = null;
+          
+          if NEW.lastapp = 'Hangup' then
+            set @source = NEW.src;
+          else
+            set @source = NEW.userfield;
+          end if;
+          
+          select (1200 - current_usage) as time_left, dids_user_phones.user_phone_id, dids_user_phones.created_at, dids_user_phones.expired
+          into @time_left, @user_phone_id, @start_date, @expired
           from dids_user_phones
           inner join dids on
           dids.id = dids_user_phones.did_id
           and dids.phone_number = NEW.dst;
+
+          if @expired = 1 then
+            set @queue_type = 1;
+          else
+            set @queue_type = 2;
+          end if;
 
           select email into @email from users
           inner join user_phones up 
@@ -21,7 +53,8 @@ class AddTriggerCallQueue < ActiveRecord::Migration
           if @email is not NULL then
             insert into call_queue set email = @email,
             calling = NEW.dst,
-            caller = NEW.userfield,
+            caller = @source,
+            queue_type = @queue_type,
             call_time = NEW.billsec,
             start_date = @start_date,
             created_at = NOW(),
