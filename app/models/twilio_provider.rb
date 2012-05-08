9152b3af238c31b48250c6fbdebbe8def3071d9e
Scott White <scott@3rdarm.com>
2012-05-08 00:13:25 -0400

    using offical twilio gem, which is WAY better

--- app/models/twilio_provider.rb
+++ app/models/twilio_provider.rb
@@ -5,14 +5,21 @@ class TwilioProvider
   PIPES_PROCESS_URL = 'http://process.pipes.io/incoming'
   
   
-  attr_accessor :did_id, :did, :sid
+  attr_accessor :did_id, :did, :sid, :account
   
   def logger
     RAILS_DEFAULT_LOGGER
   end  
   
   def initialize
-    self.class.connect
+    @connection = Twilio::REST::Client.new(SID,AUTH_TOKEN)
+    @account = @connection.account
+  end
+
+
+  def generate_capability_token
+
+
   end
   
   def did
@@ -20,11 +27,8 @@ class TwilioProvider
   end
   
   def order_did(did)
-    # {"TwilioResponse"=>{"IncomingPhoneNumber"=>{"Sid"=>"PN7acbb73ec9e30540474c762a5e2857f7", "AccountSid"=>"AC584a27ef4e2a013398ad27b5bcdb16a3", "FriendlyName"=>"(410) 514-6084", "PhoneNumber"=>"+14105146084", "VoiceUrl"=>nil, "VoiceMethod"=>"POST", "VoiceFallbackUrl"=>nil, "VoiceFallbackMethod"=>"POST", "VoiceCallerIdLookup"=>"false", "DateCreated"=>"Thu, 07 Apr 2011 00:06:59 +0000", "DateUpdated"=>"Thu, 07 Apr 2011 00:06:59 +0000", "SmsUrl"=>nil, "SmsMethod"=>"POST", "SmsFallbackUrl"=>nil, "SmsFallbackMethod"=>"POST", "Capabilities"=>{"Voice"=>"true", "SMS"=>"true"}, "StatusCallback"=>nil, "StatusCallbackMethod"=>nil, "ApiVersion"=>"2010-04-01", "Uri"=>"/2010-04-01/Accounts/AC584a27ef4e2a013398ad27b5bcdb16a3/IncomingPhoneNumbers/PN7acbb73ec9e30540474c762a5e2857f7"}}}
-    response = Twilio::IncomingPhoneNumber.create(:PhoneNumber=>did)
-    r = parse_response(response.parsed_response)
-    logger.info(response.parsed_response.inspect)
-    r['IncomingPhoneNumber']
+    response = @account.incoming_phone_numbers.create(:phone_number => did)
+    response
   end
   
   def parse_response(response)
@@ -37,40 +41,35 @@ class TwilioProvider
   end
 
   def get_did(sid)
-    response = Twilio::IncomingPhoneNumber.get(sid)
-    r = parse_response(response.parsed_response)
-    logger.info(r.inspect)
-    r
+    @account.incoming_phone_numbers.get(sid)
   end
   
   def set_voice_url(sid, id=nil)
     @sid = sid
-    self.update(sid, :VoiceUrl=>self.voice_url(id))
+    self.update(sid, :voice_url=>self.voice_url(id))
   end
   
   def update(sid,options={})
-    response = Twilio::IncomingPhoneNumber.update(sid,options)
-    r = parse_response(response.parsed_response)
-    logger.info(r.inspect)
-    r
+    response = @account.incoming_phone_numbers.get(sid).update(options)
+    response
   end
   
   def convert_number(number)
     number.gsub(/^\+1/,'')
   end
   
-  def order(number,did=nil)
+  def order(number)
     count = (count)?count+1:0
     logger.debug("order: count is #{count}")
-    first_did = did.blank? ? available_dids(number).first : did
+    first_did = did.blank? ? available_dids(number).first
     
     raise "Unable to get DIDs" if first_did.blank?
-    raise "Unable to order" unless order = order_did(first_did['PhoneNumber'])
+    raise "Unable to order" unless order = order_did(first_did.phone_number)
     
-    did_number = convert_number(first_did['PhoneNumber'])
-    d = Did.new(phone_number: did_number,usage_state: Did::ACTIVE, state: first_did['Region'], city: first_did['RateCenter'], provider: 'twilio', provider_id: order['Sid'])
+    did_number = convert_number(first_did.phone_number)
+    d = Did.new(phone_number: did_number,usage_state: Did::ACTIVE, state: first_did.region, city: first_did.rate_center, provider: 'twilio', provider_id: order.sid)
     d.save!
-    self.set_voice_url(order['Sid'],d.id)
+    self.set_voice_url(order.sid,d.id)
     d
   rescue => e
     logger.error("order: #{e.message}")
@@ -82,11 +81,7 @@ class TwilioProvider
   end  
     
   def available_dids(number)
-    dids = Twilio::AvailablePhoneNumbers.search_local(near_number: number)
-    # retarded xml, WHY? TODO: roll own, this is stupid
-    x = parse_response(dids.parsed_response)["AvailablePhoneNumbers"]['AvailablePhoneNumber']
-    logger.debug("available_dids: #{x.inspect}")
-    x
+    @account.available_phone_numbers.get('US').local.list(near_number: number)
   end
   
   
@@ -96,7 +91,7 @@ class TwilioProvider
   end
   
   def self.connect
-    @connection ||= Twilio.connect(SID,AUTH_TOKEN)
+    @connection ||= Twilio::REST::Client.new(SID,AUTH_TOKEN)
   end
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem 'aasm', version: '2.1.5'
   config.gem 'nokogiri'
   config.gem 'savon', version: '0.8.1'
-  config.gem 'twilio', version: '3.0.1'
+  config.gem 'twilio-ruby', version: '3.7.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
