ea2c9a31c4ac5a75bc41b8e600f119ab5a6b4b9e
Scott White <scott@3rdarm.com>
2012-06-25 10:20:32 -0400

    remove +1

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -1,8 +1,8 @@
 class CallLog < ActiveRecord::Base
   named_scope :by_pipes_number, lambda{|number|
-    unless number =~ /^\+1/
-      number = "+1#{number}"
-    end
+    # unless number =~ /^\+1/
+    #   number = "+1#{number}"
+    # end
     return {} if number.blank?
     {conditions: {pipes_number: number}}
   }
