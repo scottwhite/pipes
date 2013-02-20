a0f6eb02d9ba1c0bb7e9148cdd28890e380c07f3
Scott White <scott@3rdarm.com>
2013-02-20 16:21:17 -0500

    raise on deliver errors

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ActionMailer::Base
   # ActionMailer::Base.delivery_method = :sendmail
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.default_charset = "utf-8"
   ActionMailer::Base.smtp_settings = {
       :address => "email-smtp.us-east-1.amazonaws.com",
