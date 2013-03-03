f98a5fa2dc05dbb38a9f0a1de21d2c82e6248260
Scott White <scott@3rdarm.com>
2013-03-02 20:35:45 -0500

    google seed data, fixing phone number gig

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+    def is_mobile?
+        return (/Mobile/.match(request.env["HTTP_USER_AGENT"]) != nil)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,56 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 # 
-dids = [
-  {
-    phone_number: '4434589920',
-    state: 'md',
-    city: 'annapolis',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434825307',
-    state: 'md',
-    city: 'annapolis',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434513858',
-    state: 'md',
-    city: 'baltimore',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434513859',
-    state: 'md',
-    city: 'baltimore',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434513962',
-    state: 'md',
-    city: 'baltimore',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434513968',
-    state: 'md',
-    city: 'baltimore',
-    usage_state: Did::ACTIVE
-  },
-  {
-    phone_number: '4434514932',
-    state: 'md',
-    city: 'baltimore',
-    usage_state: Did::ACTIVE
-  }
-]
-
-# dids.each do|did|
-#   d = Did.find_or_create_by_phone_number(did[:phone_number])
-#   d.update_attributes!(did)
-# end
-
 
 {
   1=> {p: 3.00, n: 'new', s: 'web', i: '', a:false, t: 'PIPES_NUMBER'}, 
@@ -63,7 +13,10 @@ dids = [
   3=>{p: 2.00, n: 'Re-up',  s: 'web', i:'', a:true, t: 'REUP_20MINS'},
   4=> {p: 2.99, n: 'IOS Pipes Number', s: 'apple', i: 'pipesnumber', a:false, t: 'PIPES_NUMBER'}, 
   5=> {p: 0.99, n: 'IOS 30 minute extension', s: 'apple', i:'pipesextension', a:true, t: 'PIPES_EXTEND'}, 
-  6=>{p: 1.99, n: 'IOS 20 Minute Re-up',  s: 'apple', i:'pipesminutes', a:true, t: 'REUP_20MINS'}
+  6=>{p: 1.99, n: 'IOS 20 Minute Re-up',  s: 'apple', i:'pipesminutes', a:true, t: 'REUP_20MINS'},
+  7=> {p: 3.00, n: 'Pipes Number', s: 'google', i: 'pipes_number', a:false, t: 'PIPES_NUMBER'}, 
+  8=> {p: 1.00, n: '30 minute extension', s: 'google', i:'pipes_extend_30mins', a:true, t: 'PIPES_EXTEND'}, 
+  9=>{p: 2.00, n: 'Re-up',  s: 'google', i:'pipes_reup_default', a:true, t: 'REUP_20MINS'}
 }.each do |k,v|
   p = Product.new(price: v[:p], name: v[:n],  source: v[:s],
     source_product_id: v[:i], requires_existing: v[:a],
